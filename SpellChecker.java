e6e64226b9af047bf4a9e8fdb231b3b84b7137bc
Arthur Garcia Torrezani <torrezaniarthur@gmail.com>
2023-11-03 17:42:51 -0300

    Até a 2

---
+++ SpellChecker.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+/**
+ * Escreva uma descrição da classe SpellChecker aqui.
+ * 
+ * @author (seu nome)
+ * @version (um número da versão ou uma data)
+ */
+public class SpellChecker {
+    private DictReader dicionario;
+
+    // recebe o arquivo de texto com as palavras
+    public SpellChecker(DictReader dicionario) {
+        this.dicionario = dicionario;
+    }
+
+    // conta a quantidade de palavras
+    public int numberOfWords() {
+        int cont = 0;
+        for (String word : dicionario.getDictionary()) {
+            cont++;
+        }
+        return cont;
+    }
+
+    // verifica se a palvra solicitada existe no arquivo
+    public boolean isKnownWord(String palavra) {
+        boolean igual = false;
+        for (String word : dicionario.getDictionary()) {
+            if (word.equals(palavra)) {
+                igual = true;
+            }
+        }
+        return igual;
+    }
+
+    // verifica se todas as palavras passadas por um array estao no arquivo
+    public boolean allKnown(ArrayList<String> words) {
+        boolean igual = false;
+        for (String wordi : words) {
+            for (String word : dicionario.getDictionary()) {
+                if (wordi.equals(word)) {
+                    igual = true;
+                } else {
+                    igual = false;
+                    break;
+                }
+            }
+        }
+        return igual;
+    }
+
+    public ArrayList<String> wordsStartingWith(String prefix) {
+        ArrayList<String> palavras = new ArrayList<String>();
+
+        for (String word : dicionario.getDictionary()) {
+            String formatedWord = word.toLowerCase();
+            String formatedPrefix = prefix.toLowerCase();
+            
+            String result = "";
+            for (int i = 0; i < prefix.length(); i++) {
+                result += formatedWord.charAt(i);
+            }
+            if (result.equals(formatedPrefix)) {
+                palavras.add(word);
+            }
+        }
+        return palavras;
+    }
+
+}
